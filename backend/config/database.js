ccfb66c71414fb21ca9d75c3a35e22d7025a836b
Proyect <arieldiaz.sistemas@gmail.com>
2025-08-18 21:59:39 -0300

    backend config, database and segurity. stil not finished

---
+++ backend/config/database.js
@@ -0,0 +1,19 @@
+import 'dotenv/config.js';
+
+export default {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+    },  
+}
+;
